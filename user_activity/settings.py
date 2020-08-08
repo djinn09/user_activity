dd9b0b43c005f994269d8c8d666b0fbcea169ec4
krunal <krunalh321@gmail.com>
2020-08-08 16:21:18 +0530

    Debug change

--- user_activity/settings.py
+++ user_activity/settings.py
@@ -26,9 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config("SECRET_KEY")
 
-DATABASES = {}
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
