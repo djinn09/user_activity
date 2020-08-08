bae11a3dad3e2174d5d9288ce3bf699a34b9dfd9
krunal <krunalh321@gmail.com>
2020-08-08 16:16:38 +0530

    DEbug False

--- user_activity/settings.py
+++ user_activity/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DATABASES = {}
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
